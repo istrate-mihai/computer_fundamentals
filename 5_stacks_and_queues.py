f7092fbf8241b822c3da61d366646d0f644c0bb0
istrate-mihai <i.mihai9960@gmail.com>
2026-05-22 12:41:21 +0300

    feat: finished pointer structures chapter

--- 5_stacks_and_queues.py
+++ 5_stacks_and_queues.py
@@ -1,7 +1,16 @@
+from random import randint
+import time
+
 class Node:
     def __init__(self, data = None):
         self.data = data
         self.next = None
+
+class Node_Doubly:
+    def __init__(self, data = None, prev = None, next = None):
+        self.data = data
+        self.prev = prev
+        self.next = next
 
 class Stack:
     def __init__(self):
@@ -68,6 +77,124 @@ sl = (
  "{(foo)(bar)}[hello](((this)is)a)test))"
 )
 
-for s in sl:
-    m = check_brackets(s)
-    print("{}: {}".format(s, m))
+# for s in sl:
+#     m = check_brackets(s)
+#     print("{}: {}".format(s, m))
+
+# List based queues
+class ListQueue:
+    def __init__(self):
+        self.items = []
+        self.size  = 0
+
+    def enqueue(self, data):
+        self.items.append(0, data)
+        self.size += 1
+
+    def dequeue(self):
+        data = self.items.pop()
+        self.size -= 1
+        return data
+
+# Stack based queues
+class StackQueue:
+    def __init__(self):
+        self.inbound_stack  = []
+        self.outbound_stack = []
+
+    def enqueue(self, data):
+        self.inbound_stack.append(data)
+
+    def dequeue(self):
+        if not self.outbound_stack:
+            while self.inbound_stack:
+                data = self.inbound_stack.pop()
+                self.outbound_stack.append(data)
+
+        return self.outbound_stack.pop()
+
+# queue = StackQueue()
+# queue.enqueue(5)
+# queue.enqueue(6)
+# queue.enqueue(7)
+# print(queue.inbound_stack)
+# queue.dequeue()
+# print(queue.inbound_stack)
+# print(queue.outbound_stack)
+# queue.dequeue()
+# print(queue.outbound_stack)
+
+# Node based queues
+# A(head) <-> B(tail)
+class NodeQueue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def enqueue(self, data):
+        new_node = Node_Doubly(data, None, None)
+
+        if self.tail is None:
+            self.head = new_node
+            self.tail = self.head
+        else:
+            new_node.prev  = self.tail
+            self.tail.next = new_node
+            self.tail      = new_node
+        
+        self.count += 1
+
+    def dequeue(self):
+        current = self.head
+        
+        if self.count == 1:
+            self.head = None
+            self.tail = None
+        elif self.count > 1:
+            self.head = self.head.next
+            self.head.prev = None
+
+        self.count -= 1
+        return current
+
+# Applications of queues
+class Track:
+
+    def __init__(self, title = None):
+        self.title = title
+        self.length = randint(5, 10)
+
+track_1 = Track("white whistle")
+track_2 = Track("butter butter")
+print(track_1.length)
+print(track_2.length)
+
+class MediaPlayerQueue(NodeQueue):
+    
+    def __init__(self):
+        super(MediaPlayerQueue, self).__init__()
+
+    def add_track(self, track):
+        self.enqueue(track)
+
+    def play(self):
+        while self.count > 0:
+            current_track_node = self.dequeue()
+            print("Now playing {}".format(current_track_node.data.title))
+            time.sleep(current_track_node.data.length)
+
+track1 = Track("white whistle")
+track2 = Track("butter butter")
+track3 = Track("Oh black star")
+track4 = Track("Watch that chicken")
+track5 = Track("Don't go")
+print(track1.length)
+print(track2.length)
+media_player = MediaPlayerQueue()
+media_player.add_track(track1)
+media_player.add_track(track2)
+media_player.add_track(track3)
+media_player.add_track(track4)
+media_player.add_track(track5)
+media_player.play()
