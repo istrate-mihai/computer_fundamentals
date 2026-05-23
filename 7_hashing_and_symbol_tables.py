1fca0fd1972a1fbfe52dab498d937edbf9d5e668
istrate-mihai <i.mihai9960@gmail.com>
2026-05-23 14:02:55 +0300

    feat: added file for hash table chapter

---
+++ 7_hashing_and_symbol_tables.py
@@ -0,0 +1,68 @@
+class HashItem:
+    def __init__(self, key, value):
+        self.key   = key
+        self.value = value
+
+class HashTable:
+    def __init__(self):
+        self.size  = 256
+        self.slots = [None for i in range(self.size)]
+        self.count = 0
+
+    def _hash(self, key):
+        hv   = 0
+        mult = 1
+
+        for ch in key:
+            hv += mult * ord(ch)
+            mult += 1
+        
+        return hv % self.size
+
+    def put(self, key, value):
+        item = HashItem(key, value)
+        h    = self._hash(key)
+
+        while self.slots[h] is not None:
+            if self.slots[h] == key:
+                break
+            h = (h + 1) % self.size
+
+        if self.slots[h] is None:
+            self.count += 1    
+        self.slots[h] = item
+
+    def get(self, key):
+        h = self._hash(key)
+
+        while self.slots[h] is not None:
+            if self.slots[h].key == key:
+                return self.slots[h].value
+            h = (h + 1) % self.size
+        
+        return None
+
+    def __setitem__(self, key, value):
+        self.put(key, value)
+    
+    def __getitem(self, key, value):
+        return self.get(key)
+
+# ht = HashTable()
+# ht.put("good", "eggs")
+# ht.put("better", "ham")
+# ht.put("best", "spam")
+# ht.put("ad", "do not")
+
+ht = HashTable()
+ht["good"]   = "eggs"
+ht["better"] = "ham"
+ht["best"]   = "spam"
+ht["ad"]     = "do not"
+ht["ga"]     = "collide"
+
+for key in ("good", "better", "best", "worst", "ad", "ga"):
+    v = ht.get(key)
+    print(v)
+
+print("The number of elements is: {}".format(ht.count))
